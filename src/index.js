75aa457fb25ba414aa5765478bc56f93a3e9bdbd
Lasse S. Haslev <lasse@haslev.no>
2017-03-12 22:18:28 +0100

    Removed unused files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import BaseGeocoder from './components/BaseGeocoder';
 import Geocoder from './components/Geocoder';
-import BaseInputTrigger from './components/BaseInputTrigger';
-import InputTrigger from './components/InputTrigger';
 import UsesGoogleApi from './components/mixins/UsesGoogleApi';
 
 export default BaseGeocoder;
 export {
     BaseGeocoder,
     Geocoder,
-
-    BaseInputTrigger,
-    InputTrigger,
 
     UsesGoogleApi,
 }
